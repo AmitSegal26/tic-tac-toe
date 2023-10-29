69693cfd8a072c784363610f91d8334cc03e96c4
Amit <amitseg2612@gmail.com>
2023-10-29 11:00:48 +0200

    removing fix to gh-pages routing

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,14 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import ROUTES from "./ROUTES";
 import Game from "../pages/Game";
 import GameRandom from "../pages/GameRandom";
-import Page404 from "../pages/Page404";
-import CheckErrorRoute from "./CheckErrorRoute";
 const Router = () => {
   return (
     <Routes>
       <Route path={ROUTES.BOT} element={<GameRandom />} />
       <Route path={ROUTES.GAME} element={<Game />} />
-      <Route path="*" element={<CheckErrorRoute element={<Page404 />} />} />
     </Routes>
   );
 };
